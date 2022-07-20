30e609563078a5e4590f550536468e41e101d226
DaichiOwlz <109676428+DaichiOwlz@users.noreply.github.com>
2022-07-20 21:30:02 +0700

    Add files via upload

---
+++ Fast Food Restaurant Website/Function.js
@@ -0,0 +1,23 @@
+function myFunction(){
+	alert("You have submitted your comments");
+}
+
+function Tenderloin(){
+	alert("You have selected this item");
+}
+
+function Ribeye(){
+	alert("You have selected this item");
+}
+
+function Sirloin(){
+	alert("You have selected this item");
+}
+
+function Tbone(){
+	alert("You have selected this item");
+}
+
+function Last(){
+	var payment = prompt(" Please pay your money here you'll get your food in less than 40 minutes");
+}
